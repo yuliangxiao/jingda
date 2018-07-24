ed33b42759a56046dfc5acb46015e1f81ef2ecd5
于良宵 <1095139010@qq.com>
2018-07-24 16:16:10 +0800

    11

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NavParams } from 'ionic-angular';
+import { Http, Response } from '@angular/http';
+
 
 @Component({
   selector: 'page-contact',
@@ -8,7 +10,16 @@ import { NavParams } from 'ionic-angular';
 })
 export class ContactPage {
   public params: Text;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
     this.params = this.navParams.get('title');
+    this.ionViewDidLoad();
   }
+  ionViewDidLoad() {
+    // 网络请求
+    this.http.request('http://127.0.0.1:8888/GetBLNo')
+      .subscribe((res: Response) => {
+        console.log(res);
+      });
+  }
+
 }
