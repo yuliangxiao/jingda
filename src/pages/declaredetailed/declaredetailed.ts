d3b1256153a48b19888a421ad928229a0fc54055
于良宵 <1095139010@qq.com>
2018-07-26 14:19:12 +0800

    123

--- src/pages/declaredetailed/declaredetailed.ts
+++ src/pages/declaredetailed/declaredetailed.ts
@@ -17,21 +17,29 @@ export class DeclaredetailedPage {
   public file: any;
   public blid: number;
   public items: any;
+  public is_first: boolean;
   constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
     this.params = this.navParams.get('title');
+    this.is_first = false;
   }
   getItems(ev: any) {
-
     let val = ev.target.value;
-
     if (val && val.trim() != '') {
-
-      this.http.request('http://127.0.0.1:8888/GetBLList')
+      this.http.request('http://127.0.0.1:8888/GetBLList?keyword=' + val)
         .toPromise()
         .then(res => {
-          this.items = res;
+          this.items = res.json();
+          if (this.items.length == 0) {
+            this.is_first = true;
+          }
+          else {
+            this.is_first = false;
+          }
         })
         .catch(err => { console.error(err) });
+    }
+    else{
+      this.items = null;
     }
   }
 }
